c4c7a2dc187a56a3b6b7a455fd6ee2dea422ca2f
adong <adong_yogee@163.com>
2018-01-15 15:23:35 +0800

    format

--- src/main/java/com/yogee/admin/controller/LoginController.java
+++ src/main/java/com/yogee/admin/controller/LoginController.java
@@ -8,11 +8,8 @@ package com.yogee.admin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
 
 @Controller
 @RequestMapping("/a")
